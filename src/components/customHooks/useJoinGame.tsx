8fbb8c3af1aeaf6aa8d1a3659412566448fee3b4
Joao Lucas <ramossuarezjl@gmail.com>
2024-02-18 10:22:47 -0300

    add axios

---
+++ src/components/customHooks/useJoinGame.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useJoinGame = (game_uuid: string) => {
+	useEffect(() => {
+		const response = fetch("", { method: "POST" });
+	}, []);
+};
